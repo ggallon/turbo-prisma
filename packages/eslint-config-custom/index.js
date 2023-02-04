2c28f8e36d8b98105c4c901a033ffc3fdcca49be
Gwenaël Gallon <gwenael.gallon@mac.com>
2023-02-04 18:21:44 +0100

    fix remane Eslint files to .js

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,12 +1,12 @@
 module.exports = {
   extends: ["next", "turbo", "prettier"],
-  settings: {
-    next: {
-      rootDir: ["apps/*/", "packages/*/"],
-    },
-  },
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+  },
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve("next/babel")],
+    },
   },
 };
