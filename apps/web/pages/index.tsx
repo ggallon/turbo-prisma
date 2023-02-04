794369d9aeebff4ef82a15efa5aaf509e23c24af
Gwenaël Gallon <gwenael.gallon@mac.com>
2023-02-04 23:43:58 +0100

    feat add logos of tech

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -10,12 +10,39 @@ const VARIANTS = [
   { variant: "subtle" },
 ];
 
+const LOGOS = [
+  { alt: "Turborepo", file: "turborepo-icon-light-background.svg" },
+  { alt: "Pnpm", file: "pnpm-icon-light-background.svg" },
+  { alt: "Next.js", file: "nextjs-icon-light-background.svg" },
+  { alt: "Tailwind CSS", file: "tailwindcss-icon.svg" },
+  { alt: "Prisma", file: "prisma-icon-light-background.svg" },
+  { alt: "PlanetScale", file: "planetscale-icon-light-background.svg" },
+];
+
 export default function Web() {
   return (
     <Layout>
-      <div className="flex min-h-screen flex-col items-center justify-center  gap-y-10">
-        <h1 className="ext-4xl font-extrabold lg:text-5xl">Starter</h1>
-        <a href="/api/users">[API] go to view Users</a>
+      <div className="flex min-h-screen flex-col items-center justify-center gap-y-10">
+        <div className="mb-10">
+          <h1 className="text-4xl font-extrabold lg:text-5xl">
+            Monorepo Starter
+          </h1>
+          <div className="flex flex-row justify-center items-center gap-x-5 mt-5">
+            {LOGOS.map((logo, id) => (
+              <div key={logo.alt}>
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  src={`/logos/${logo.file}`}
+                  alt={logo.alt}
+                  className={logo.alt === "Tailwind CSS" ? " w-7" : "h-6"}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <a href="/api/users">
+          <span className="font-bold">[API]</span> go to view Users
+        </a>
         <div className="space-x-3">
           {VARIANTS.map((version: any) => (
             <Button
@@ -27,14 +54,6 @@ export default function Web() {
             </Button>
           ))}
         </div>
-        <ul className="space-x-3">
-          <li>Monorepo</li>
-          <li>PNPM</li>
-          <li>TURBO</li>
-          <li>Nextjs</li>
-          <li>Prisam</li>
-          <li>Tailwind</li>
-        </ul>
       </div>
     </Layout>
   );
