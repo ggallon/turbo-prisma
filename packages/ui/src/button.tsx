fd965b63697650dcd86b97ac9d6991d92bead894
Gwenaël Gallon <gwenael.gallon@mac.com>
2023-02-05 03:07:57 +0100

    Clean Button component

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -10,21 +10,41 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { LoadingDots } from "./loading-dots";
 
 export const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 dark:hover:text-slate-100 disabled:opacity-50 dark:focus:ring-slate-400 disabled:pointer-events-none dark:focus:ring-offset-slate-900 data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800",
+  [
+    "inline-flex items-center justify-center rounded-md font-medium transition-colors",
+    "disabled:opacity-50 disabled:pointer-events-none",
+    "focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2",
+    "dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900",
+    "data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800",
+  ],
   {
     variants: {
       variant: {
-        default:
-          "bg-slate-900 text-white hover:bg-slate-700 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-800",
-        negative:
-          "bg-red-500 text-white hover:bg-red-600 dark:hover:bg-red-600",
-        outline:
-          "bg-transparent border border-slate-200 hover:bg-slate-100 dark:border-slate-700 dark:text-slate-100",
-        subtle:
-          "bg-slate-100 text-slate-900 hover:bg-slate-200 dark:bg-slate-700 dark:text-slate-100",
-        ghost:
-          "bg-transparent hover:bg-slate-100 dark:hover:bg-slate-800 dark:text-slate-100 dark:hover:text-slate-100 data-[state=open]:bg-transparent dark:data-[state=open]:bg-transparent",
-        link: "bg-transparent underline-offset-4 hover:underline text-slate-900 dark:text-slate-100 hover:bg-transparent dark:hover:bg-transparent",
+        default: [
+          "bg-black hover:bg-slate-700",
+          "dark:bg-mono-light dark:text-slate-900 dark:hover:bg-black dark:hover:text-mono-light",
+        ],
+        negative: [
+          "bg-red-500 hover:bg-red-600",
+          "dark:hover:bg-red-600",
+        ],
+        outline: [
+          "bg-transparent border border-slate-200 hover:bg-slate-100",
+          "dark:border-slate-700 dark:text-slate-100 dark:hover:text-slate-700",
+        ],
+        subtle: [
+          "bg-slate-100 text-slate-900 hover:bg-slate-200",
+          "dark:bg-slate-700 dark:text-slate-100 dark:hover:text-slate-700",
+        ],
+        ghost: [
+          "bg-transparent hover:bg-slate-100",
+          "dark:hover:bg-slate-800 dark:text-slate-100 dark:hover:text-slate-100",
+          "data-[state=open]:bg-transparent dark:data-[state=open]:bg-transparent",
+        ],
+        link: [
+          "bg-transparent underline-offset-4 hover:underline text-slate-900",
+          "dark:text-slate-100 hover:bg-transparent dark:hover:bg-transparent",
+        ],
       },
       size: {
         default: "h-10 px-3 text-sm",
